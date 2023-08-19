46551dd87d266b5addeeefff1662e2fab1c84451
Eva <as45986@gmail.com>
2023-08-19 14:29:31 +0800

    add google auth,and install nodemon

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
+const keys = require('./config/keys');
 
-app.get('/', (req, res)=>{
-  res.send({ hi: 'there'});
-})
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientId,
+  clientSecret: keys.googleClientSecret,
+  //add a route handler to our express application to handle a user coming back to our application on this route '/auth/google/callback'.
+  callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done ) => {
+  //Access and Refresh Tokens
+  console.log('accessToken',accessToken);
+  console.log('refreshToken',refreshToken);
+  console.log('profile',profile);
+}));
+
+// Google auth
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT);
-
-
-
-
+app.listen(PORT)
