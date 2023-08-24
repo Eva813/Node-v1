3f54b7feedfe07067494d0f005fddbf6a5fd97a0
Eva <as45986@gmail.com>
2023-08-24 20:05:51 +0800

    test for build

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 const app = express();
+const keys = require('./config/keys');
 require('./services/passport');
 //require('./routes/authRoutes') 會回傳函式，再加入app參數，直接呼叫
 require('./routes/authRoutes')(app);
+
+mongoose.connect(keys.mongoURI);
 
 app.get('/', (req, res) => {
   res.send({ hi: 'there' });
